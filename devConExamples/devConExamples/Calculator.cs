fc68b5ef38b6586dacea115aae68e8786119a350
Scott Schmerer <s.schmerer@techsmith.com>
2013-10-07 14:30:06 -0400

    +Ability to Add to Calculator

--- devConExamples/devConExamples/Calculator.cs
+++ devConExamples/devConExamples/Calculator.cs
@@ -1,6 +1,27 @@
-﻿namespace devConExamples
+﻿using System.Collections.Generic;
+
+namespace devConExamples
 {
    public class Calculator
    {
+      private List<IOperation> _actions;
+
+      public int RunEvaulations()
+      {
+         int result = 0;
+         foreach ( var action in _actions )
+         {
+            result = action.Evaluate( result );
+         }
+
+         return result;
+      }
+
+      public void AddThis( int newNumber )
+      {
+         var adder = new Adder( newNumber );
+
+         _actions.Add( adder );
+      }
    }
 }
