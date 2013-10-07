02fd5b6e915fa6e88db78445e3d161b43ebffbc5
Scott Schmerer <s.schmerer@techsmith.com>
2013-10-07 15:59:32 -0400

    Implement Evaluate in Multiplier

--- devConExamples/devConExamples/Multiplier.cs
+++ devConExamples/devConExamples/Multiplier.cs
@@ -2,8 +2,16 @@
 {
    public class Multiplier : IOperation
    {
+      private int _operand;
+
+      public Multiplier( int operand )
+      {
+         _operand = operand;
+      }
+
       public int Evaluate( int incomingOperand )
       {
+         return incomingOperand * _operand;
       }
    }
 }
