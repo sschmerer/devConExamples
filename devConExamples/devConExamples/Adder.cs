574690ed6dedc57361b4bf4abd58c2c4dc2b32b5
Scott Schmerer <s.schmerer@techsmith.com>
2013-10-07 14:12:12 -0400

    +Empty Adder, not-implemented

---
+++ devConExamples/devConExamples/Adder.cs
@@ -0,0 +1,11 @@
+﻿
+namespace devConExamples
+{
+   public class Adder : IOperation
+   {
+      public int Evaluate( int left, int right )
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
