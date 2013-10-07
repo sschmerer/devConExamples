1cec2a45ea3fbead151cf675cd04fe21e4f95684
Scott Schmerer <s.schmerer@techsmith.com>
2013-10-07 15:44:28 -0400

    +Multiple, Divide, Undo, and Redo

---
+++ devConExamples/devConExamples/Divider.cs
@@ -0,0 +1,17 @@
+﻿namespace devConExamples
+{
+   public class Divider : IOperation
+   {
+      private int _operand;
+
+      public Divider( int operand )
+      {
+         _operand = operand;
+      }
+
+      public int Evaluate( int incomingOperand )
+      {
+         return incomingOperand / _operand;
+      }
+   }
+}
