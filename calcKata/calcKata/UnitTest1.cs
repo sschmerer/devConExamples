c46fa05eb507e61c5a2c048bbec52a931bd5d192
William J Scanlon IV <w.scanlon@techsmith.com>
2013-10-09 07:08:54 -0400

    Create kata project

---
+++ calcKata/calcKata/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace calcKata
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+         string input = "";
+         int result = returnZero(input);
+         Assert.AreEqual( 0, result );
+      }
+
+      private int returnZero( string input )
+      {
+         return 0;
+      }
+   }
+}
