604b6f41e3821d87cb388301e608476994c86dc5
William J Scanlon IV <w.scanlon@techsmith.com>
2013-10-09 08:44:14 -0400

    input second number after operator

--- InterviewCalc/InterviewCalc/MainWindow.xaml.cs
+++ InterviewCalc/InterviewCalc/MainWindow.xaml.cs
@@ -15,14 +15,25 @@ namespace InterviewCalc
       }
 
       private int _firstNumber = 0;
+      private int _secondNumber = 0;
       private string _operator;
 
       void onClickNumber( object sender, RoutedEventArgs e )
       {
-         int digit = int.Parse( ( (Button) sender ).Content.ToString() );
-         _firstNumber = (_firstNumber*10) + digit;
+         string displayString;
+         Button button = (Button) sender;
+         if ( string.IsNullOrEmpty( _operator ) )
+         {
+            _firstNumber = appendButtonContentAsDigitToNumber( button, _firstNumber );
+            displayString = _firstNumber.ToString();
+         }
+         else
+         {
+            _secondNumber = appendButtonContentAsDigitToNumber( button, _secondNumber );
+            displayString = _firstNumber.ToString() + _operator + _secondNumber.ToString();
+         }
 
-         TheDisplay.Content = _firstNumber.ToString();
+         TheDisplay.Content = displayString;
       }
 
       void onClickOperator( object sender, RoutedEventArgs e )
@@ -30,6 +41,12 @@ namespace InterviewCalc
          _operator = ( (Button) sender ).Content.ToString();
 
          TheDisplay.Content = _firstNumber.ToString() + _operator;
+      }
+
+      int appendButtonContentAsDigitToNumber( Button button, int number )
+      {
+         int digit = int.Parse( button.Content.ToString() );
+         return (number*10) + digit;
       }
    }
 }
